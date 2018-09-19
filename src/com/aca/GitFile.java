526026b4dd4bc200602f83682745a4f4eb561bcd
calderrick <43256002+calderrick@users.noreply.github.com>
2018-09-18 20:10:50 -0500

    Ignore these files!!
    updated everytime-- irrelelvant

---
+++ src/com/aca/GitFile.java
@@ -0,0 +1,12 @@
+package com.aca;
+
+public class GitFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//This is just to see if I can share this class
+	}
+	
+
+}
